1886e4b77d3dc7ede725fc321290259076d54eb3
Gaziwang <gaziwang0@gmail.com>
2025-06-15 21:49:09 +0800

    compelite

--- Day_1.py
+++ Day_1.py
@@ -4,4 +4,23 @@
 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         
+        return []
+        
+#哈希表方法
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
+        
+"""
